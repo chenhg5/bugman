a6f6af19ffe7ea5a8ff08196bea1eaa9cc64b419
cg33 <chg80333@163.com>
2018-10-22 17:17:31 +0800

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bugger
+
+- 每隔五分钟拉取所有host的error.log
+- 比较error.log大小
---
+++ main.go
@@ -0,0 +1,101 @@
+package bugger
+
+import (
+	"github.com/bramvdbogaerde/go-scp/auth"
+	"github.com/bramvdbogaerde/go-scp"
+	"fmt"
+	"os"
+	"golang.org/x/crypto/ssh"
+	"ecsDeploy/deploy"
+	"utils/sms"
+	"time"
+	"utils/ini"
+	"flag"
+)
+
+var Sizes map[string]int64
+
+func main() {
+
+	var configPath string
+	flag.StringVar(&configPath, "config", "./config/config.ini", "config path")
+
+	config, _ := ini.Get(configPath, "bugger")
+	dayuCfg, _ := ini.Get(configPath, "dayu")
+	ecsCfg, _ := ini.Get(configPath, "ecs")
+	project := deploy.NewProject(ecsCfg)
+
+	smser := sms.InitAlidayu(dayuCfg["key"], dayuCfg["secret"], dayuCfg["sign"], dayuCfg["code"])
+
+	// 初始化Size
+	ips := project.GetIps()
+	for i := 0; i < len(ips); i++ {
+		// 复制
+		localFile := "/root/logcenter/" + ips[i] + "/" + config["local_file"]
+		Copy("root", config["ssh_key"], ips[i], config["remote_file"], localFile, config["local_file_permission"])
+		fileInfo, _ := os.Stat(localFile)
+		Sizes[ips[i]] = fileInfo.Size()
+	}
+
+	for true {
+		// 每隔五分钟进行一次
+		time.Sleep(time.Minute * 8)
+
+		// 阿里云获取最新的host
+		ips := project.GetIps()
+
+		// 从 host 拉取最新的error.log
+		for i := 0; i < len(ips); i++ {
+			go func(host string) {
+				// 复制
+				localFile := "/root/logcenter/" + host + "/" + config["local_file"]
+				Copy("root", config["ssh_key"], host, config["remote_file"], localFile, config["local_file_permission"])
+				// 检查大小
+				if !CheckSize(localFile, host) {
+					// 如果有增加就发通知
+					Notify(smser, host, config["phone"])
+				}
+			}(ips[i])
+		}
+	}
+}
+
+func CheckSize(file string, host string) bool {
+	fileInfo, _ := os.Stat(file)
+	if size, ok := Sizes[host]; ok {
+		if fileInfo.Size() != size {
+			Sizes[host] = fileInfo.Size()
+			return false
+		}
+		return true
+	} else {
+		Sizes[host] = fileInfo.Size()
+		return true
+	}
+}
+
+func Notify(smser *sms.AlidayuSmSType, host string, phone string) {
+	smser.SendAlidayuSMS(phone, host)
+}
+
+func Copy(user, key, host, remoteFile, localFile, permission string) error {
+	clientConfig, _ := auth.PrivateKey(user, key, ssh.InsecureIgnoreHostKey())
+
+	client := scp.NewClient(host, &clientConfig)
+
+	// Connect to the remote server
+	err := client.Connect()
+	if err != nil {
+		fmt.Println("Couldn't establisch a connection to the remote server ", err)
+		return err
+	}
+
+	// Open a file
+	f, _ := os.Open(localFile)
+	defer client.Close()
+	defer f.Close()
+
+	client.CopyFile(f, remoteFile, permission) // 0655
+
+	return nil
+}
